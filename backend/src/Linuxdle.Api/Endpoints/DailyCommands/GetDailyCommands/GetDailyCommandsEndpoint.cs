1a73ea4e366c1205977f3425342370a5af5e470d
Youssef Tarek <yousseftarekali04@gmail.com>
2026-02-22 23:58:27 +0200

    feat: add GetDailyCommands endpoint with caching

---
+++ backend/src/Linuxdle.Api/Endpoints/DailyCommands/GetDailyCommands/GetDailyCommandsEndpoint.cs
@@ -0,0 +1,22 @@
+using Linuxdle.Services.DailyCommands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Linuxdle.Api.Endpoints.DailyCommands.GetDailyCommands;
+
+internal sealed class GetDailyCommandsEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/daily-commands", HandleAsync)
+        .CacheOutput(policy => policy.Expire(TimeSpan.FromHours(24)));
+    }
+
+    private async Task<IResult> HandleAsync(
+        [FromServices] IDailyCommandService dailyCommandService,
+        CancellationToken cancellationToken)
+    {
+        var dailyCommandNames = await dailyCommandService.GetDailyCommandsAsync(cancellationToken);
+
+        return Results.Ok(dailyCommandNames);
+    }
+}
