a076997224608bbb54cd5cc8f95790f0057b2132
Youssef Tarek <yousseftarekali04@gmail.com>
2026-02-27 03:53:00 +0200

    feat: implement RefreshUserEndpoint for token refresh functionality

---
+++ backend/src/Linuxdle.Api/Endpoints/Users/RefreshUser/RefreshUserEndpoint.cs
@@ -0,0 +1,37 @@
+using System.Reflection.Metadata;
+using Linuxdle.Api.Extensions;
+using Linuxdle.Services.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Linuxdle.Api.Endpoints.Users.RefreshUser;
+
+internal sealed class RefreshUserEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/users/refresh-token", HandleAsync)
+        .WithTags(Tags.Users);
+    }
+
+    private async Task<IResult> HandleAsync(
+        [FromServices] IUserService userService,
+        HttpContext context,
+        IOptions<RefreshTokenOptions> options,
+        CancellationToken cancellationToken)
+    {
+        var refreshTokenOptions = options.Value;
+        string? oldRefreshToken = context.Request.Cookies[refreshTokenOptions.CookieName];
+
+        if (oldRefreshToken is null)
+        {
+            return Results.Unauthorized();
+        }
+
+        var tokens = await userService.RefreshUserToken(oldRefreshToken, cancellationToken);
+
+        context.Response.AppendRefreshToken(tokens.RefreshToken, refreshTokenOptions);
+
+        return Results.Ok(tokens.AccessToken);
+    }
+}
