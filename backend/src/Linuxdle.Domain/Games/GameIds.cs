69956e777a12e8d2bb73a2318b7080433797765d
Youssef Tarek <yousseftarekali04@gmail.com>
2026-03-02 22:43:44 +0200

    Add GameIds constants class to eliminate magic numbers

---
+++ backend/src/Linuxdle.Domain/Games/GameIds.cs
@@ -0,0 +1,10 @@
+namespace Linuxdle.Domain.Games;
+
+public static class GameIds
+{
+    public const int DailyCommands = 1;
+
+    public const int DailyDistros = 2;
+
+    public const int DailyDesktopEnvironments = 3;
+}
