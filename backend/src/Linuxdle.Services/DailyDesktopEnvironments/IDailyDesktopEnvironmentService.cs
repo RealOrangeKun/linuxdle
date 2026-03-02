277185faf09ad7cb7f800097805c28648666aa70
Youssef Tarek <yousseftarekali04@gmail.com>
2026-03-02 03:37:22 +0200

    Add IDailyDesktopEnvironmentService interface

---
+++ backend/src/Linuxdle.Services/DailyDesktopEnvironments/IDailyDesktopEnvironmentService.cs
@@ -0,0 +1,9 @@
+using Linuxdle.Services.Dtos.Records;
+
+namespace Linuxdle.Services.DailyDesktopEnvironments;
+
+public interface IDailyDesktopEnvironmentService
+{
+    Task<DailyDesktopEnvironmentGuessResultDto> HandleUserGuessAsync(string userGuess, CancellationToken cancellationToken = default);
+    Task<byte[]> GetDailyDesktopEnvironmentScreenshot(CancellationToken cancellationToken = default);
+}
