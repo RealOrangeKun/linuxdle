5eb12a0896f1f6e1bd77e790d697af35b1df4521
Youssef Tarek <yousseftarekali04@gmail.com>
2026-03-06 22:01:18 +0200

    feat(backend): add GetDailyDesktopEnvironments endpoint to retrieve daily desktop environments

---
+++ backend/src/Linuxdle.Api/Endpoints/DailyDesktopEnvironments/GetDailyDesktopEnvironments/GetDailyDesktopEnvironmentsEndpoint.cs
@@ -0,0 +1,23 @@
+using Linuxdle.Services.DailyDesktopEnvironments;
+using Linuxdle.Services.Dtos.Records;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Linuxdle.Api.Endpoints.DailyDesktopEnvironments.GetDailyDesktopEnvironments;
+
+internal sealed class GetDailyDesktopEnvironmentsEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/daily-desktop-environments", HandleAsync)
+        .WithTags(Tags.DailyDesktopEnvironments);
+    }
+
+    private async Task<IResult> HandleAsync(
+        [FromServices] IDailyDesktopEnvironmentService dailyDesktopEnvironmentService,
+        CancellationToken cancellationToken)
+    {
+        var result = await dailyDesktopEnvironmentService.GetDailyDesktopEnvironmentsAsync(cancellationToken);
+
+        return Results.Ok(result);
+    }
+}
